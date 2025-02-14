aab5d27508061060652cb60db8972737f5fb7557
barsa0412 patro <barsapatro0412@gmail.com>
2025-02-14 21:10:39 +0000

    tourdetails

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,7 +1,63 @@
 import React from "react"; //6.9k (gzipped: 2.7k)
+import "../styles/tour-details.css";
+import { Container, Row, Col, Form, ListGroup } from "reactstrap";
+import { useParams } from "react-router-dom";
+import tourData from "../assets/data/tours";
+import calculateAvgRating from "../utils/avgRating";
 
 const TourDetails = () => {
-    return <div>TourDetails</div>;
+    const {id} = useParams()
+    //this is an static data later we will call our api and load our data from database
+
+    const tour = tourData.find(tour=> tour.id === id)
+    //destructor propertites from tour object
+    const {photo, title, desc, price, reviews, address, city, distance, maxGroupSize} = tour;
+
+    const {totalRating, avgRating} = calculateAvgRating(reviews)
+
+    return <>
+    <section>
+        <Container>
+            <Row>
+                <Col lg="8">
+                <div className="tour__content">
+                    <img src={photo} alt="" />
+                    <div className="tour__info">
+                        <h2>{title}</h2>
+                        <div className="d-flex align-items-center gap-5">
+                            <span className="d-flex align-items-center gap-1">
+                <i class="ri-user-star-line" style={{color : "orange"}}
+                ></i> {calculateAvgRating === 0 ? null : avgRating }
+                {totalRating === 0 ? (
+                    "Not rated" 
+                ) : (
+                     <span>({reviews.length})</span> 
+                    )}
+                </span>
+
+                <span>
+                <i class="ri-map-pin-user-fill"></i> {address}
+                </span>
+                        </div>
+                        <div className="tour__extra-details">
+                            <span> <i class="ri-map-pin-5-line"></i>{city}</span>
+                            <span> <i class="ri-price-tag-line"></i>${price} /per person</span>
+                            <span> <i class="ri-group-3-line"></i>{maxGroupSize}</span>
+
+                        </div>
+                        <h5>Description</h5>
+                        <p>{desc}</p>
+                    </div>
+                    {/* ============ tour review section =========== */}
+                    
+                    {/* ============ tour review section end =========== */}
+
+                </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    </>
 };
 
 export default TourDetails;
--- tour-management/frontend/src/pages/Tours.jsx
+++ tour-management/frontend/src/pages/Tours.jsx
@@ -1,7 +1,56 @@
-import React from "react"; //6.9k (gzipped: 2.7k)
+import React, {useState, useEffect} from "react"; //6.9k (gzipped: 2.7k)
+import CommonSection from "../shared/CommonSection";
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
+import TourCard from "./../shared/TourCard";
+import SearchBar from "./../shared/SearchBar";
+import Newsletter from "./../shared/Newsletter";
+import { Col, Container, Row,  } from "reactstrap";
+
 
 const Tours = () => {
-    return <div>Tours</div>;
+    const [pageCount, setPageCount] = useState(0)
+    const [page, setpage] = useState(0)
+
+    useEffect(() => {
+        const pages = Math.ceil(5/ 4) //letter we will use backend data count
+        setPageCount(pages);
+    },[page])
+    return (
+    <>
+    <CommonSection title = {"All Tours"} />
+    <section>
+        <Container>
+            <Row>
+                <SearchBar />
+            </Row>
+        </Container>
+    </section>
+    <section className="pt-0">
+        <Container>
+            <Row>
+                {
+                    tourData?.map(tour => (<Col lg="3" key={tour.id}>                  
+                        <TourCard tour={tour}/>
+                        </Col>
+                        ))}
+                        <Col lg="12">
+                        <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                            {[...Array(pageCount).keys()].map(number =>(
+                                <span key={number} onClick={()=> setpage(number)}
+                                className={page === number ? "active__page" : ""}
+                                >
+                                    {number + 1}
+                                </span>
+                            ))}
+                        </div>
+                        </Col>
+            </Row>
+        </Container>
+    </section>
+    
+    </>
+    );
 };
 
 export default Tours;

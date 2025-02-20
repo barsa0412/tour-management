151a6008dcb89f91e287ce90fdd4f7c5d336affd
barsa0412 patro <barsapatro0412@gmail.com>
2025-02-20 21:18:09 +0000

    authtication

--- tour-management/backend/controllers/tourController.js
+++ tour-management/backend/controllers/tourController.js
@@ -15,7 +15,7 @@ export const createTour = async (req,res) => {
 
 //update tour
 export const updateTour = async(req,res) => {
-    const id = req.params.id
+    const id = req.params.id;
     try{
         const updateTour = await Tour.findByIdAndUpdate(id, {
             $set: req.body
@@ -23,36 +23,88 @@ export const updateTour = async(req,res) => {
         
         res.status(200).json({success:true, message:"Successfully updated", data:updateTour});
     } catch(err) {
-        res.status(500).json({success:true, message:"Failed to update"});
+        res.status(500).json({success:false, message:"Failed to update"});
     }
 };
 
 
 //delete  tour
 export const deleteTour = async(req,res) => {
+    const id = req.params.id;
     try{
-
+        await Tour.findByIdAndDelete(id);
+        
+        res.status(200).json({success:true, message:"Successfully deleted"});
     } catch(err) {
-
+        res.status(500).json({success:false, message:"Failed to delete"});
     }
 };
 
 
 //getsingle tour
 export const getSingleTour = async(req,res) => {
+    const id = req.params.id;
     try{
-
+       const tour = await Tour.findById(id);
+        
+        res.status(200).json({success:true, message:"Successful", data: tour});
     } catch(err) {
-
+        res.status(404).json({success:false, message:"not found"});
     }
 };
 
 
 //getall tour
 export const getAllTour = async(req,res) => {
+
+    //for pagination
+    const page = parseInt(req.query.page);
+
+    try{
+        const tours = await Tour.find({}).skip(page * 8).limit(8);
+        res.status(200).json({success:true, count: tours.length, message:"Successful", data: tours});
+    } catch(err) {
+        res.status(404).json({success:false, message:"not found"});
+    }
+};
+
+
+//get search by tour
+export const getTourBySearch = async(req,res) => {
+ //here "i" means case sensitive
+    const city = new RegExp(req.query.city, "i");
+    const distance = parseInt(req.query.distance);
+    const maxGroupSize = parseInt(req.query.maxGroupSize);
+
     try{
 
+        //gte means greater thn equal
+        const tours = await Tour.find({city, distance: {$gte: distance},
+        maxGroupSize: {$gte: maxGroupSize} });
+        res.status(200).json({success:true, count: tours.length, message:"Successful", data: tours});
     } catch(err) {
+        res.status(404).json({success:false, message:"not found"});
 
+    }
+};
+
+//get featured tour
+export const getFeaturedTour = async(req,res) => {
+
+    try{
+        const tours = await Tour.find({featured:true}).limit(8);
+        res.status(200).json({success:true, message:"Successful", data: tours});
+    } catch(err) {
+        res.status(404).json({success:false, message:"not found"});
+    }
+};
+
+//get tour counts
+export const getTourCount = async(req,res) =>{
+    try{
+        const tourCount = await Tour.estimatedDocumentCount();
+        res.status(200).json({success:true, data:tourCount});
+    }catch(err) {
+        res.status(500).json({success:false, message:"failed to fetch"});
     }
 };
--- tour-management/backend/routes/tours.js
+++ tour-management/backend/routes/tours.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTour, deleteTour, getAllTour, getSingleTour, updateTour } from "../controllers/tourController.js";
+import { createTour, deleteTour, getAllTour, getFeaturedTour, getSingleTour, getTourBySearch, getTourCount, updateTour } from "../controllers/tourController.js";
 
 const router = express.Router();
 
@@ -9,14 +9,18 @@ router.post("/", createTour);
 // update tour 
 router.put("/:id", updateTour);
 
-// create new tour 
+// delete tour 
 router.delete("/:id", deleteTour);
 
-// create new tour 
+// get single tour 
 router.get("/:id", getSingleTour);
 
-// create new tour 
+// get all tour
 router.get("/", getAllTour);
 
+//get tour by search
+router.get("/search/getTourBySearch", getTourBySearch);
+router.get("/search/getFeaturedTours", getFeaturedTour);
+router.get("/search/getTourCount", getTourCount);
 
 export default router;
